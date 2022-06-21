4969e06058bd4f6f29da9b739c8779c7242517c7
mmich-pl <m.michal.parzych@gmail.com>
2022-06-21 18:14:49 +0200

    replace simple buffer by tokio BufReader

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,24 @@
-use tokio::{io::{AsyncReadExt, AsyncWriteExt}, net::TcpListener};
+use tokio::{
+    io::{AsyncWriteExt, AsyncBufReadExt, BufReader},
+    net::TcpListener};
 
 #[tokio::main]
 async fn main() {
     let listener = TcpListener::bind("localhost:8080").await.unwrap();
     let (mut socket, _addr) = listener.accept().await.unwrap();
 
+    let (reader, mut writer) = socket.split();
+
+    let mut reader = BufReader::new(reader);
+    let mut line = String::new();
+
     loop {
-        let mut buffer = [0u8; 1024];
-        let bytes_read = socket.read(&mut buffer).await.unwrap();
+        let bytes_read = reader.read_line(&mut line).await.unwrap();
+        if bytes_read == 0 {
+            break;
+        }
         //write all bytes in buffer, not send to all connected sockets
-        socket.write_all(&buffer[..bytes_read]).await.unwrap();
+        writer.write_all(line.as_bytes()).await.unwrap();
+        line.clear();
     }
 }
