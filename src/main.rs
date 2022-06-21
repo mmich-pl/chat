28c1ba39c9a7591ccc7ea1cec6017f7cf2648571
mmich-pl <m.michal.parzych@gmail.com>
2022-06-21 18:04:13 +0200

    create simple tcp echo server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use tokio::{io::{AsyncReadExt, AsyncWriteExt}, net::TcpListener};
+
+#[tokio::main]
+async fn main() {
+    let listener = TcpListener::bind("localhost:8080").await.unwrap();
+    let (mut socket, _addr) = listener.accept().await.unwrap();
+
+    loop {
+        let mut buffer = [0u8; 1024];
+        let bytes_read = socket.read(&mut buffer).await.unwrap();
+        //write all bytes in buffer, not send to all connected sockets
+        socket.write_all(&buffer[..bytes_read]).await.unwrap();
+    }
 }
