d28ca53a5a5115ae7ed12d166387c3a18f196197
mmich-pl <m.michal.parzych@gmail.com>
2022-06-22 14:12:08 +0200

    add support for multimple clients

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,29 @@
 use tokio::{
     io::{AsyncWriteExt, AsyncBufReadExt, BufReader},
-    net::TcpListener};
+    net::TcpListener,
+};
 
 #[tokio::main]
 async fn main() {
     let listener = TcpListener::bind("localhost:8080").await.unwrap();
-    let (mut socket, _addr) = listener.accept().await.unwrap();
-
-    let (reader, mut writer) = socket.split();
-
-    let mut reader = BufReader::new(reader);
-    let mut line = String::new();
 
     loop {
-        let bytes_read = reader.read_line(&mut line).await.unwrap();
-        if bytes_read == 0 {
-            break;
-        }
-        //write all bytes in buffer, not send to all connected sockets
-        writer.write_all(line.as_bytes()).await.unwrap();
-        line.clear();
+        let (mut socket, _addr) = listener.accept().await.unwrap();
+        tokio::spawn(async move{
+            let (reader, mut writer) = socket.split();
+
+            let mut reader = BufReader::new(reader);
+            let mut line = String::new();
+
+            loop {
+                let bytes_read = reader.read_line(&mut line).await.unwrap();
+                if bytes_read == 0 {
+                    break;
+                }
+                //write all bytes in buffer, not send to all connected sockets
+                writer.write_all(line.as_bytes()).await.unwrap();
+                line.clear();
+            }
+        });
     }
 }
